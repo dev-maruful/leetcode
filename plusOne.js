576f0709aef1e7dd9b75a29ceb85320d2b3dbb58
MD Maruful Islam <maruful.islam401@gmail.com>
2024-08-28 09:25:59 +0600

    broken solution added

--- plusOne.js
+++ plusOne.js
@@ -11,15 +11,15 @@
 // Explanation: The array represents the integer 123.
 // Incrementing by one gives 123 + 1 = 124.
 // Thus, the result should be [1,2,4].
-// Example 2:
 
+// Example 2:
 // Input: digits = [4,3,2,1]
 // Output: [4,3,2,2]
 // Explanation: The array represents the integer 4321.
 // Incrementing by one gives 4321 + 1 = 4322.
 // Thus, the result should be [4,3,2,2].
-// Example 3:
 
+// Example 3:
 // Input: digits = [9]
 // Output: [1,0]
 // Explanation: The array represents the integer 9.
@@ -30,4 +30,29 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function (digits) {};
+var plusOne = function (digits) {
+  let arrToStr = digits.join("");
+
+  console.log(arrToStr, parseInt(arrToStr));
+  let num = parseInt(arrToStr) + 1;
+  let numToStr = num.toString();
+  let arr = [];
+
+  for (let i = 0; i < numToStr.length; i++) {
+    parseInt(numToStr[i]);
+    arr.push(numToStr[i]);
+  }
+
+  let arr2 = [...arr];
+  arr = [];
+
+  for (let j = 0; j < arr2.length; j++) {
+    arr.push(parseInt(arr2[j]));
+  }
+
+  console.log(arr);
+
+  return arr;
+};
+
+plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]);
